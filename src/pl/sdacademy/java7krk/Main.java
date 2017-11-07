660a9eaecf7291669518e7266d5dcc91c4c1ec7c
Aneta <anetaryznar@gmail.com>
2017-11-07 21:27:37 +0100

    ini1

--- src/pl/sdacademy/java7krk/Main.java
+++ src/pl/sdacademy/java7krk/Main.java
@@ -1,8 +1,26 @@
 package pl.sdacademy.java7krk;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        List<String> list = new LinkedList<String>();
+
+        for (int i = 0; i < 4; i++) {
+
+
+            System.out.println("podaj imiona");
+            String imiona = scanner.nextLine();
+            list.add(imiona);
+        }
+
+        List<String > fefe = WordsMixer.mixWords(list);
+        System.out.println(fefe);
+
+
     }
 }
---
+++ src/pl/sdacademy/java7krk/WordsMixer.java
@@ -0,0 +1,20 @@
+package pl.sdacademy.java7krk;
+
+import java.util.Collections;
+import java.util.List;
+
+public class WordsMixer {
+
+    static List<String> mixWords (List<String> lista){
+        Collections.shuffle(lista);
+        return lista;
+
+
+
+    }
+
+
+
+
+
+}
